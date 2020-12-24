44293e5d65f2aeaa04c53a4ee85e38c7dc110e35
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-12-25 02:18:59 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './Components/NavBar.js'
+import InfoPanel from './Components/InfoPanel.js'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <NavBar/>
+      <InfoPanel/>
+    </div>
+    )
+}
+export default App;
